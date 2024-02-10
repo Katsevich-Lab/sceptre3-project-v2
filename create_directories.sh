743c6f510f730831acc2d2724e6df37f66c80488
Tim Barry <tbarry2@andrew.cmu.edu>
2024-02-09 20:24:33 -0500

    set analysis params of sceptre_object

---
+++ create_directories.sh
@@ -0,0 +1,2 @@
+source ~/.research_config
+mkdir -p $LOCAL_SCEPTRE3_DATA_DIR/replogle-2022/rd7
---
+++ rd7_analysis/set_analysis_parameters.R
@@ -0,0 +1,22 @@
+library(sceptre)
+repl_offsite <- paste0(.get_config_path("LOCAL_REPLOGLE_2022_DATA_DIR"))
+sceptre3_rd7_offsite <- paste0(.get_config_path("LOCAL_SCEPTRE3_DATA_DIR"), "replogle-2022/rd7/")
+import_dir <- paste0(repl_offsite, "processed/rd7/")
+sceptre_object <- read_ondisc_backed_sceptre_object(sceptre_object_fp = paste0(import_dir, "sceptre_object.rds"),
+                                                    response_odm_file_fp = paste0(import_dir, "gene.odm"),
+                                                    grna_odm_file_fp = paste0(import_dir, "grna.odm"))
+# update the parameters of this sceptre_object
+positive_control_pairs <- construct_positive_control_pairs(sceptre_object)
+discovery_pairs <- construct_trans_pairs(sceptre_object = sceptre_object,
+                                         positive_control_pairs = positive_control_pairs,
+                                         pairs_to_exclude = "pc_pairs")
+targets_to_study <- unique(discovery_pairs$grna_target)[1:100]
+discovery_pairs <- discovery_pairs |> dplyr::filter(grna_target %in% targets_to_study)
+sceptre_object <- set_analysis_parameters(sceptre_object,
+                                          discovery_pairs = discovery_pairs,
+                                          positive_control_pairs = positive_control_pairs,
+                                          formula_object = formula(~ grna_n_nonzero + grna_n_umis + response_n_nonzero + response_n_umis))
+
+# save the sceptre_object with analysis parameters set
+write_ondisc_backed_sceptre_object(sceptre_object = sceptre_object,
+                                   directory_to_write = sceptre3_rd7_offsite)
