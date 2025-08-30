ab265f63c35c60d77b3a942296c6045b04db7532
jdeu1023 <jdeu@wharton.upenn.edu>
2025-08-30 00:00:59 -0400

    make test gasperini data

--- benchmarking/guide-assignment/data-preprocessing/convert_odm_to_h5ad.R
+++ benchmarking/guide-assignment/data-preprocessing/convert_odm_to_h5ad.R
@@ -1,7 +1,10 @@
 
 
-#' library(reticulate)
-#' library(anndata)
+library(reticulate)
+# library(anndata)
+library(SingleCellExperiment)
+library(zellkonverter)
+
 #' 
 #' conda_create("r-anndata", packages = c("python=3.12"))
 #' use_condaenv("r-anndata", required = TRUE)
@@ -54,13 +57,14 @@ row_to_sparse_matrix <- function(values) {
   )
 }
 
-odm_to_R <- function(odm) {
+odm_to_R <- function(odm, num_rows=NULL) {
   if(!inherits(odm, "odm")) {
     stop("`odm` must be an object of class odm as defined in the ondisc package.")
   }
-  out <- lapply(1:nrow(odm), function(i) odm[i,] |> row_to_sparse_matrix()) |>
+  num_rows_to_read <- ifelse(is.null(num_rows), nrow(odm), num_rows)
+  out <- lapply(1:num_rows_to_read, function(i) odm[i,] |> row_to_sparse_matrix()) |>
     do.call(what = rbind)
-  rownames(out) <- rownames(odm)
+  rownames(out) <- rownames(odm)[1:num_rows_to_read]
   return(out)
 }
 
@@ -76,82 +80,67 @@ R_to_h5ad <- function(mat, path_to_write) {
   writeH5AD(sce, path_to_write)
 }
 
-# making a small piece of gasperini
-gasperini_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/grna.odm")
-gasperini_mat <- odm_to_R(gasperini_odm)
-dim(gasperini_mat)
-
-gasp_small <- gasperini_mat[1:500, 1:1000]
-
-library(reticulate)
-
-conda_create("r-anndata", packages = c("python=3.12"))
-use_condaenv("r-anndata", required = TRUE)
-py_install(c("numpy", "scipy", "h5py", "anndata"), #"zarr")
-           envname = "r-anndata", pip = TRUE)
-py_config()
-R_to_h5ad(gasp_small, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini/grna_small.h5ad")
 
 
-
-library(reticulate)
-
-conda_create("r-anndata", packages = c("python=3.12"))
-use_condaenv("r-anndata", required = TRUE)
-py_install(c("numpy", "scipy", "h5py", "anndata"), #"zarr")
-           envname = "r-anndata", pip = TRUE)
-py_config()
-
-#' write_h5ad_from_odm_in_memory
 #' 
-#' Note that this function loads the entire odm file into memory.
-#'
-#' @param odm : an odm object as defined by the ondisc package
-#' @param path_to_write : a string giving the path to write the resulting h5ad
-#' @param chunk_size : the rows of `odm` are initially grouped into batches of this size
-write_h5ad_from_odm_in_memory <- function(odm, path_to_write, chunk_size = 100) {
-  if(!inherits(odm, "odm")) {
-    stop("`odm` must be an object of class odm as defined in the ondisc package.")
-  }
-  suppressPackageStartupMessages({
-    library(Matrix)
-    library(SingleCellExperiment)
-    library(zellkonverter)
-  })
-  
-  # helper func to make each row of the odm into a 1 x num_cells dgRMatrix
-  row_to_sparse_matrix <- function(values) {
-    nonzero <- which(values != 0)
-    Matrix::sparseMatrix(
-      i = rep(1L, length(nonzero)),  # it's all one row
-      j = nonzero,          
-      x = values[nonzero],                
-      dims = c(1L, length(values)),
-      repr = "R"
-    )
-  }
-  
-  n_vars <- nrow(odm)   
-  n_obs <- ncol(odm)  
-  n_chunks <- ceiling(n_vars / chunk_size)
-  chunk_list <- vector("list", n_chunks)
-  # each chunk gets turned into a dgRMatrix before they all get rbind'd together
-  for (i in seq_len(n_chunks)) {
-    idx_start <- (i - 1) * chunk_size + 1
-    idx_end   <- min(i * chunk_size, n_vars)
-    chunk_list[[i]] <- lapply(idx_start:idx_end, function(j) odm[j,] |> row_to_sparse_matrix()) |>
-      do.call(what = rbind)
-  }
-  
-  out_delayed <- do.call(rbind, chunk_list)
-  rownames(out_delayed) <- rownames(odm)
-  # ondisc doesn't let these be accessed, so we will make our own
-  colnames(out_delayed) <- paste0("CELL_", seq_len(n_obs))
-  
-  # Wrap & write (zellkonverter streams blocks to .h5ad)
-  sce <- SingleCellExperiment(assays = list(counts = out_delayed))
-  writeH5AD(sce, path_to_write)
-}
+#' library(reticulate)
+#' 
+#' conda_create("r-anndata", packages = c("python=3.12"))
+#' use_condaenv("r-anndata", required = TRUE)
+#' py_install(c("numpy", "scipy", "h5py", "anndata"), #"zarr")
+#'            envname = "r-anndata", pip = TRUE)
+#' py_config()
+#' 
+#' #' write_h5ad_from_odm_in_memory
+#' #' 
+#' #' Note that this function loads the entire odm file into memory.
+#' #'
+#' #' @param odm : an odm object as defined by the ondisc package
+#' #' @param path_to_write : a string giving the path to write the resulting h5ad
+#' #' @param chunk_size : the rows of `odm` are initially grouped into batches of this size
+#' write_h5ad_from_odm_in_memory <- function(odm, path_to_write, chunk_size = 100) {
+#'   if(!inherits(odm, "odm")) {
+#'     stop("`odm` must be an object of class odm as defined in the ondisc package.")
+#'   }
+#'   suppressPackageStartupMessages({
+#'     library(Matrix)
+#'     library(SingleCellExperiment)
+#'     library(zellkonverter)
+#'   })
+#'   
+#'   # helper func to make each row of the odm into a 1 x num_cells dgRMatrix
+#'   row_to_sparse_matrix <- function(values) {
+#'     nonzero <- which(values != 0)
+#'     Matrix::sparseMatrix(
+#'       i = rep(1L, length(nonzero)),  # it's all one row
+#'       j = nonzero,          
+#'       x = values[nonzero],                
+#'       dims = c(1L, length(values)),
+#'       repr = "R"
+#'     )
+#'   }
+#'   
+#'   n_vars <- nrow(odm)   
+#'   n_obs <- ncol(odm)  
+#'   n_chunks <- ceiling(n_vars / chunk_size)
+#'   chunk_list <- vector("list", n_chunks)
+#'   # each chunk gets turned into a dgRMatrix before they all get rbind'd together
+#'   for (i in seq_len(n_chunks)) {
+#'     idx_start <- (i - 1) * chunk_size + 1
+#'     idx_end   <- min(i * chunk_size, n_vars)
+#'     chunk_list[[i]] <- lapply(idx_start:idx_end, function(j) odm[j,] |> row_to_sparse_matrix()) |>
+#'       do.call(what = rbind)
+#'   }
+#'   
+#'   out_delayed <- do.call(rbind, chunk_list)
+#'   rownames(out_delayed) <- rownames(odm)
+#'   # ondisc doesn't let these be accessed, so we will make our own
+#'   colnames(out_delayed) <- paste0("CELL_", seq_len(n_obs))
+#'   
+#'   # Wrap & write (zellkonverter streams blocks to .h5ad)
+#'   sce <- SingleCellExperiment(assays = list(counts = out_delayed))
+#'   writeH5AD(sce, path_to_write)
+#' }
 
 # for testing
 # my_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/code/sceptre-examples/nextflow-example-data/response.odm")
@@ -159,6 +148,6 @@ write_h5ad_from_odm_in_memory <- function(odm, path_to_write, chunk_size = 100) 
 
 
 # for real
-gasperini_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/grna.odm")
-dim(gasperini_odm)
-write_h5ad_from_odm_in_memory(gasperini_odm, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini/grna.h5ad", 50)
+# gasperini_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/grna.odm")
+# dim(gasperini_odm)
+# write_h5ad_from_odm_in_memory(gasperini_odm, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini/grna.h5ad", 50)
---
+++ benchmarking/guide-assignment/data-preprocessing/make_gasperini_small.R
@@ -0,0 +1,48 @@
+source("~/katsevich-lab/code/sceptre3-project-v2/benchmarking/guide-assignment/data-preprocessing/convert_odm_to_h5ad.R")
+
+# making a small piece of gasperini
+grna_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/grna.odm")
+num_grnas <- 500
+num_cells <- 1000
+grna_mat <- odm_to_R(grna_odm, num_rows = num_grnas)
+dim(grna_mat)
+
+grna_small <- grna_mat[,1:num_cells]
+
+
+# making the h5ad files for pertpy and crispat
+
+library(reticulate)
+
+conda_create("r-anndata", packages = c("python=3.12"))
+use_condaenv("r-anndata", required = TRUE)
+py_install(c("numpy", "scipy", "h5py", "anndata"), #"zarr")
+           envname = "r-anndata", pip = TRUE)
+py_config()
+R_to_h5ad(grna_small, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/grna_counts_crispat.h5ad")
+R_to_h5ad(grna_small, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/grna_counts_pertpy.h5ad")
+
+
+# making the mtx file for CLEANSER
+Matrix::writeMM(grna_small, "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/grna_counts_cleanser.mtx")
+
+
+# making sceptre's files
+grna_small |>
+  as.matrix() |>
+  as.data.frame() |>
+  `colnames<-`(paste0("CELL_", 1:ncol(grna_small))) |>
+  write.csv("~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/sceptre_input/grna_matrix.csv")
+
+
+response_odm <- ondisc::initialize_odm_from_backing_file("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/response.odm")
+response_small <- odm_to_R(response_odm, num_rows = 100)[,1:num_cells]
+response_small |>
+  as.matrix() |>
+  as.data.frame() |>
+  `colnames<-`(paste0("CELL_", 1:ncol(grna_small))) |>
+  write.csv("~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/sceptre_input/response_matrix.csv")
+
+scep <- readRDS("~/katsevich-lab/data/external/gasperini-2019-v3/at-scale/processed/sceptre_object.rds")
+write.csv(scep@grna_target_data_frame[1:num_cells, ], "~/data/projects/sceptre3/benchmarking/guide_assignment/input_data/gasperini_small/sceptre_input/grna_target_data_frame.csv")
+
