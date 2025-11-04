5093e05f5c6f887c23c60c5e22b14a3e84aa0464
Joseph Deutsch <jdeu@wharton.upenn.edu>
2025-11-03 21:02:30 -0500

    tidy up sceptre-pipeline for replogle-rd7

---
+++ benchmarking/guide-assignment/sceptre-pipeline/replogle-rd7/run-set-analysis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#$ -N scep-pipeline_replogle-rd7_set-analysis
+#$ -j y
+#$ -cwd
+#$ -pe openmp 1              # use the existing PE
+#$ -l m_mem_free=20G         # scheduler reservation (per slot)
+
+set -euo pipefail
+
+R --vanilla -e 'source("set_analysis_parameters.R")'
