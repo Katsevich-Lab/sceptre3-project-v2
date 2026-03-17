8f89222954e3514753159af990d083a631e77e9f
Joseph Deutsch <jdeu@wharton.upenn.edu>
2026-03-17 03:19:23 -0400

    fix path issues

--- benchmarking/association/sceptre-pipeline/gasperini/run-sceptre-pipeline.sh
+++ benchmarking/association/sceptre-pipeline/gasperini/run-sceptre-pipeline.sh
@@ -17,12 +17,12 @@ data_name="gasperini"
 assoc_dataset_name="gasperini_comp_ngenes=100_ntargets=100_ncells=50k_n_nonzero_p=0.75"
 
 data_dir=$LOCAL_BENCHMARKING_DIR"guide_assignment/input_data/"$data_name"/sceptre-pipeline/"
-assoc_input_dir=$LOCAL_BENCHMARKING_DIR"association/computational/input_data/"$dataset"/sceptre-pipeline/"
+assoc_input_dir=$LOCAL_BENCHMARKING_DIR"association/computational/input_data/"$assoc_dataset_name"/sceptre-pipeline/"
 
 sceptre_object_fp=$assoc_input_dir"sceptre_object.rds"
 
 response_odm_fp=$data_dir"response.odm"
-grna_odm_fp=$data_directory"grna.odm"
+grna_odm_fp=$data_dir"grna.odm"
 
 
 output_fp=$LOCAL_BENCHMARKING_DIR"association/computational/outputs/sceptre-pipeline/"$assoc_dataset_name
