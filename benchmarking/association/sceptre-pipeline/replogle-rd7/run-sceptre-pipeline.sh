7d2a140befd44b45e5a6c8e35935805d64abaeba
jdeu1023 <jdeu@wharton.upenn.edu>
2026-03-30 01:02:15 -0400

    use 16xl.q profile

--- benchmarking/association/sceptre-pipeline/replogle-rd7/run-sceptre-pipeline.sh
+++ benchmarking/association/sceptre-pipeline/replogle-rd7/run-sceptre-pipeline.sh
@@ -41,6 +41,7 @@ cd $output_fp
 #################
 nextflow run jdeu1023/sceptre-pipeline -r main \
  -c $config_fp \
+ -profile profile_16xl \
  --sceptre_object_fp $sceptre_object_fp \
  --response_odm_fp $response_odm_fp \
  --grna_odm_fp $grna_odm_fp \
