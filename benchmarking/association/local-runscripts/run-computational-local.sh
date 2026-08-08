a4809beca8c4ee212f38c81a185812b3c6d9d828
jdeu1023 <jdeu@wharton.upenn.edu>
2026-08-08 02:05:23 -0400

    update local compute runscript

--- benchmarking/association/local-runscripts/run-computational-local.sh
+++ benchmarking/association/local-runscripts/run-computational-local.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Select which run to execute
 # RUN_ID="test_100genes"
 # RUN_ID="small_500genes"
-RUN_ID="test_sceptregampoi"
+RUN_ID="test_frpert"
 
 OUT_BASE="$HOME/data/projects/sceptre3/benchmarking/association/computational/outputs"
 OUT_DIR="${OUT_BASE}/${RUN_ID}"
